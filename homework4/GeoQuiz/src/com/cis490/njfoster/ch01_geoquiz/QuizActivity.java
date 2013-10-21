b6006ab9119d26c9e37b26a8204d4370b62717b3
Nathaniel Foster <nathanielsmusic@gmail.com>
2013-10-21 16:10:56 -0400

    Updating the controller layer

--- homework4/GeoQuiz/src/com/cis490/njfoster/ch01_geoquiz/QuizActivity.java
+++ homework4/GeoQuiz/src/com/cis490/njfoster/ch01_geoquiz/QuizActivity.java
@@ -5,23 +5,48 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends Activity {
 	private Button btnTrue;
 	private Button btnFalse;
+	private Button btnNext;
+	private TextView mQuestionTextView;
+	
+	private TrueFalse[] mQuestionBank = new TrueFalse[]{
+			new TrueFalse(R.string.question_oceans, true),
+			new TrueFalse(R.string.question_mideast, false),
+			new TrueFalse(R.string.question_africa, false),
+			new TrueFalse(R.string.question_asia, true)
+	};
+	
+	private int mCurrentIndex = 0;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
-    
+        
+        mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
+        updateQuestion();
+        
+        btnNext = (Button)findViewById(R.id.btnNext);
+        btnNext.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+				updateQuestion();
+			}
+		});
+        
         btnTrue = (Button)findViewById(R.id.btnTrue);
         btnTrue.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View arg0) {
-				Toast.makeText(QuizActivity.this, R.string.tstIncorrect, Toast.LENGTH_SHORT).show();
+				checkAnswer(true);
 			}
 		});
         
@@ -30,12 +55,29 @@ public class QuizActivity extends Activity {
 			
 			@Override
 			public void onClick(View v) {
-				Toast.makeText(QuizActivity.this, R.string.tstCorrect, Toast.LENGTH_SHORT).show();
+				checkAnswer(false);
 			}
 		});
     }
 
 
+	private void updateQuestion() {
+		int question = mQuestionBank[mCurrentIndex].getQuestion();
+        mQuestionTextView.setText(question);
+	}
+
+	private void checkAnswer(boolean userPressedTrue){
+		boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+		
+		int messageResId = 0;
+		
+		if (userPressedTrue == answerIsTrue) {
+			messageResId = R.string.tstCorrect;
+		} else {
+			messageResId = R.string.tstIncorrect;
+		}
+		Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+	}
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
