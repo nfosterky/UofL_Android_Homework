c15f379620513e85798036b51330e63ed2b31dae
Nathaniel Foster <nathanielsmusic@gmail.com>
2013-10-22 20:32:52 -0400

    added logging

--- homework4/GeoQuiz/src/com/cis490/njfoster/ch01_geoquiz/QuizActivity.java
+++ homework4/GeoQuiz/src/com/cis490/njfoster/ch01_geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.cis490.njfoster.ch01_geoquiz;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -9,6 +10,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends Activity {
+	private static final String TAG = "QuizActivity";
 	private Button btnTrue;
 	private Button btnFalse;
 	private Button btnNext;
@@ -26,6 +28,7 @@ public class QuizActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
         
         mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
@@ -59,8 +62,37 @@ public class QuizActivity extends Activity {
 			}
 		});
     }
-
-
+    
+    @Override
+    public void onStart() {
+    	super.onStart();
+    	Log.d(TAG, "onStart() called");
+    }
+    
+    @Override
+    public void onPause() {
+    	super.onPause();
+    	Log.d(TAG, "onPause() called");
+    }
+    
+    @Override
+    public void onResume() {
+    	super.onResume();
+    	Log.d(TAG, "onResume() called");
+    }
+    
+    @Override
+    public void onStop() {
+    	super.onStop();
+    	Log.d(TAG, "onStop() called");
+    }
+    
+    @Override
+    public void onDestroy() {
+    	super.onDestroy();
+    	Log.d(TAG, "onDestroy() called");
+    }
+    
 	private void updateQuestion() {
 		int question = mQuestionBank[mCurrentIndex].getQuestion();
         mQuestionTextView.setText(question);
