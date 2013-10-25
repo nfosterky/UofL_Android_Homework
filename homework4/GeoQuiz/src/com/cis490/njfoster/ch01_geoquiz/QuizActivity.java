eac6f49397fae6bb3c2b9c8d3b66faafd9a04a2a
Nathaniel Foster <nathanielsmusic@gmail.com>
2013-10-25 17:43:51 -0400

    using intent extras / enabling cheating

--- homework4/GeoQuiz/src/com/cis490/njfoster/ch01_geoquiz/QuizActivity.java
+++ homework4/GeoQuiz/src/com/cis490/njfoster/ch01_geoquiz/QuizActivity.java
@@ -75,8 +75,10 @@ public class QuizActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				Intent i = new Intent(QuizActivity.this, CheatActivity.class);
+				boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+				i.putExtra(CheatActivity.EXTRA_ANSWER_IS_TRUE, answerIsTrue);
 				startActivity(i);
-				// adddd commments
+				
 			}
 		});
     }
