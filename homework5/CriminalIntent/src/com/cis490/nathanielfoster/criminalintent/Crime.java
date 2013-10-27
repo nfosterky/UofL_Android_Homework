d592523780b172b64181c466ada3454192506270
Nathaniel Foster <nathanielsmusic@gmail.com>
2013-10-27 13:49:28 -0400

    Creating the CrimeFragment class

--- homework5/CriminalIntent/src/com/cis490/nathanielfoster/criminalintent/Crime.java
+++ homework5/CriminalIntent/src/com/cis490/nathanielfoster/criminalintent/Crime.java
@@ -9,4 +9,17 @@ public class Crime {
 	public Crime() {
 		mId = UUID.randomUUID();
 	}
+
+	public String getmTitle() {
+		return mTitle;
+	}
+
+	public void setmTitle(String mTitle) {
+		this.mTitle = mTitle;
+	}
+
+	public UUID getmId() {
+		return mId;
+	}
+	
 }
