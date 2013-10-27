19389295f95016095b9250a4578700c74854040e
Nathaniel Foster <nathanielsmusic@gmail.com>
2013-10-27 13:59:50 -0400

    Creating the crime fragment class

--- homework5/CriminalIntent/src/com/cis490/nathanielfoster/criminalintent/CrimeActivity.java
+++ homework5/CriminalIntent/src/com/cis490/nathanielfoster/criminalintent/CrimeActivity.java
@@ -1,9 +1,9 @@
 package com.cis490.nathanielfoster.criminalintent;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.view.Menu;
+import android.support.v4.app.FragmentManager;
 
 public class CrimeActivity extends FragmentActivity {
 
@@ -11,7 +11,17 @@ public class CrimeActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+    
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+        
+        if (fragment == null) {
+        	fragment = new CrimeFragment();
+        	fm.beginTransaction()
+        		.add(R.id.fragmentContainer, fragment)
+        		.commit();
+        }
     }
-
+    
     
 }
