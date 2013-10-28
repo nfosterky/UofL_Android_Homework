a0cd5acf75aaeb1ba42593bf4294121d5ea11236
Nathaniel Foster <nathanielsmusic@gmail.com>
2013-10-28 17:46:32 -0400

    reloading the list

--- homework5/CriminalIntent/src/com/cis490/nathanielfoster/criminalintent/CrimeListFragment.java
+++ homework5/CriminalIntent/src/com/cis490/nathanielfoster/criminalintent/CrimeListFragment.java
@@ -62,4 +62,10 @@ public class CrimeListFragment extends ListFragment {
 			return convertView;
 		}
 	}
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+		((CrimeAdapter)getListAdapter()).notifyDataSetChanged();
+	}
 }
