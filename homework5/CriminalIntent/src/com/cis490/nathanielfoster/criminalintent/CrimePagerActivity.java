0af2e2b8025596f1d3db4aaddfaf5d55ba92851c
Nathaniel Foster <nathanielsmusic@gmail.com>
2013-10-31 20:36:34 -0400

    chapter 11 added view pager to swipe through crimes

---
+++ homework5/CriminalIntent/src/com/cis490/nathanielfoster/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,74 @@
+package com.cis490.nathanielfoster.criminalintent;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+
+public class CrimePagerActivity extends FragmentActivity {
+	private ViewPager mViewPager;
+	private ArrayList<Crime> mCrimes;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		mViewPager = new ViewPager(this);
+		mViewPager.setId(R.id.viewPager);
+		setContentView(mViewPager);
+		
+		mCrimes = CrimeLab.get(this).getCrimes();
+		
+		FragmentManager fm = getSupportFragmentManager();
+		mViewPager.setAdapter(new FragmentStatePagerAdapter(fm) {
+
+			@Override
+			public Fragment getItem(int pos) {
+				Crime crime = mCrimes.get(pos);
+				return CrimeFragment.newInstance(crime.getId());
+			}
+
+			@Override
+			public int getCount() {
+				return mCrimes.size();
+			}
+			
+		});
+		
+		mViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+			
+			@Override
+			public void onPageSelected(int pos) {
+				Crime crime = mCrimes.get(pos);
+				if (crime.getTitle() != null) {
+					setTitle(crime.getTitle());
+				}
+			}
+			
+			@Override
+			public void onPageScrolled(int pos, float posOffset, int posOffsetPixels) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onPageScrollStateChanged(int state) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		UUID crimeId = (UUID)getIntent()
+			.getSerializableExtra(CrimeFragment.EXTRA_CRIME_ID);
+		for (int i = 0; i < mCrimes.size(); i++) {
+			if (mCrimes.get(i).getId().equals(crimeId)) {
+				mViewPager.setCurrentItem(i);
+				break;
+			}
+		}
+	}
+}
