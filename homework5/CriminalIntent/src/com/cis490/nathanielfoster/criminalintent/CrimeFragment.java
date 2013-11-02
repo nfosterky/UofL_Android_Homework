bbe3c1a26415b32d8b20b42585981cbdaf116576
Nathaniel Foster <nathanielsmusic@gmail.com>
2013-11-02 19:28:37 -0400

    Finishing up ch12, Passing data between two fragments

--- homework5/CriminalIntent/src/com/cis490/nathanielfoster/criminalintent/CrimeFragment.java
+++ homework5/CriminalIntent/src/com/cis490/nathanielfoster/criminalintent/CrimeFragment.java
@@ -1,7 +1,10 @@
 package com.cis490.nathanielfoster.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -20,6 +23,7 @@ public class CrimeFragment extends Fragment {
 	public static final String EXTRA_CRIME_ID 
 		= "com.cis490.nathanielfoster.criminalintent.crime_id";
 	private static final String DIALOG_DATE = "date";
+	private static final int REQUEST_DATE = 0;
 	
 	private Crime mCrime;
 	private EditText mTitleField;
@@ -62,13 +66,14 @@ public class CrimeFragment extends Fragment {
 		});
 		
 		mDateButton = (Button)v.findViewById(R.id.crime_date);
-		mDateButton.setText(mCrime.getDate().toString());
+		updateDate();
 		mDateButton.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 				FragmentManager fm = getActivity().getSupportFragmentManager();
 				DatePickerFragment dialog = DatePickerFragment.newInstance(mCrime.getDate());
+				dialog.setTargetFragment(CrimeFragment.this, REQUEST_DATE);
 				dialog.show(fm, DIALOG_DATE);	
 			}
 		});
@@ -82,6 +87,22 @@ public class CrimeFragment extends Fragment {
 			}
 		});
 		return v;
+	}
+	
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (resultCode != Activity.RESULT_OK) return;
+		
+		if (requestCode == REQUEST_DATE) {
+			Date date = (Date)data
+					.getSerializableExtra(DatePickerFragment.EXTRA_DATE);
+			mCrime.setDate(date);
+			updateDate();
+		}
+	}
+
+	private void updateDate() {
+		mDateButton.setText(mCrime.getDate().toString());
 	}
 	
 	public static CrimeFragment newInstance(UUID crimeId) {
--- homework5/CriminalIntent/src/com/cis490/nathanielfoster/criminalintent/DatePickerFragment.java
+++ homework5/CriminalIntent/src/com/cis490/nathanielfoster/criminalintent/DatePickerFragment.java
@@ -4,8 +4,11 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.View;
@@ -28,6 +31,17 @@ public class DatePickerFragment extends DialogFragment {
 		return fragment;
 	}
 	
+	private void sendResult(int resultCode) {
+		if (getTargetFragment() == null) 
+			return;
+		
+		Intent i = new Intent();
+		i.putExtra(EXTRA_DATE, mDate);
+		
+		getTargetFragment()
+			.onActivityResult(getTargetRequestCode(), resultCode, i);
+	}
+	
 	@Override
 	public Dialog onCreateDialog(Bundle savedInstanceState) {
 		mDate = (Date)getArguments().getSerializable(EXTRA_DATE);
@@ -36,7 +50,7 @@ public class DatePickerFragment extends DialogFragment {
 		calendar.setTime(mDate);
 		int year = calendar.get(Calendar.YEAR);
 		int month = calendar.get(Calendar.MONTH);
-		int day = calendar.get(calendar.DAY_OF_MONTH);
+		int day = calendar.get(Calendar.DAY_OF_MONTH);
 		
 		View v = getActivity().getLayoutInflater()
 				.inflate(R.layout.dialog_date, null);
@@ -57,7 +71,12 @@ public class DatePickerFragment extends DialogFragment {
 		return new AlertDialog.Builder(getActivity())
 			.setView(v)
 			.setTitle(R.string.date_picker_title)
-			.setPositiveButton(android.R.string.ok, null)
+			.setPositiveButton(android.R.string.ok, 
+				new DialogInterface.OnClickListener() {		
+					public void onClick(DialogInterface dialog, int which) {
+						sendResult(Activity.RESULT_OK);
+					}
+			 	})
 			.create();
 	}
 }
