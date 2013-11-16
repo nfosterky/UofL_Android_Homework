fa469910a82b2c202da668c34f4fd9529e218853
Nathaniel Foster <nathanielsmusic@gmail.com>
2013-11-16 18:31:54 -0500

    An Alternative Menu File

--- homework5/CriminalIntent/src/com/cis490/nathanielfoster/criminalintent/CrimeListFragment.java
+++ homework5/CriminalIntent/src/com/cis490/nathanielfoster/criminalintent/CrimeListFragment.java
@@ -2,9 +2,12 @@ package com.cis490.nathanielfoster.criminalintent;
 
 import java.util.ArrayList;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -17,6 +20,7 @@ import android.widget.TextView;
 
 public class CrimeListFragment extends ListFragment {
 	private ArrayList<Crime> mCrimes;
+	private boolean mSubtitleVisible;
 	private static final String TAG = "CrimeListFragment";
 	
 	@Override 
@@ -29,6 +33,9 @@ public class CrimeListFragment extends ListFragment {
 		
 		CrimeAdapter adapter = new CrimeAdapter(mCrimes);
 		setListAdapter(adapter);
+		
+		setRetainInstance(true);
+		mSubtitleVisible = false;
 	}
 	
 	@Override
@@ -78,21 +85,50 @@ public class CrimeListFragment extends ListFragment {
 	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
 		super.onCreateOptionsMenu(menu, inflater);
 		inflater.inflate(R.menu.fragment_crime_list, menu);
+		MenuItem showSubtitle = menu.findItem(R.id.menu_item_show_subtitle);
+		if (mSubtitleVisible && showSubtitle != null) {
+			showSubtitle.setTitle(R.string.hide_subtitle);
+		}
 	}
 
+	@TargetApi(11)
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
-		case R.id.menu_item_new_crime:
-			Crime crime = new Crime();
-			CrimeLab.get(getActivity()).addCrime(crime);
-			Intent i = new Intent(getActivity(), CrimePagerActivity.class);
-			i.putExtra(CrimeFragment.EXTRA_CRIME_ID, crime.getId());
-			startActivityForResult(i,0);
-			return true;
-
-		default:
-			return super.onOptionsItemSelected(item);
+			case R.id.menu_item_new_crime:
+				Crime crime = new Crime();
+				CrimeLab.get(getActivity()).addCrime(crime);
+				Intent i = new Intent(getActivity(), CrimePagerActivity.class);
+				i.putExtra(CrimeFragment.EXTRA_CRIME_ID, crime.getId());
+				startActivityForResult(i,0);
+				return true;
+			case R.id.menu_item_show_subtitle:
+				if (getActivity().getActionBar().getSubtitle() == null) {
+					getActivity().getActionBar().setSubtitle(R.string.subtitle);
+					mSubtitleVisible = true;
+					item.setTitle(R.string.hide_subtitle);
+				} else {
+					getActivity().getActionBar().setSubtitle(null);
+					item.setTitle(R.string.show_subtitle);
+					mSubtitleVisible = false;
+				}
+				
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
 		} 
+	}
+	
+	@TargetApi(11)
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+		View v = super.onCreateView(inflater, parent, savedInstanceState);
+		
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			if (mSubtitleVisible) {
+				getActivity().getActionBar().setSubtitle(R.string.subtitle);
+			}
+		}
+		return v;
 	}
 }
