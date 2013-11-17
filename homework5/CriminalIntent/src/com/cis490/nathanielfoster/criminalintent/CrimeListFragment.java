ca27656fdc6cc036b2d6f5131d63d5b767ac5c99
Nathaniel Foster <nathanielsmusic@gmail.com>
2013-11-16 19:22:48 -0500

    Implementing a Floating Context Menu

--- homework5/CriminalIntent/src/com/cis490/nathanielfoster/criminalintent/CrimeListFragment.java
+++ homework5/CriminalIntent/src/com/cis490/nathanielfoster/criminalintent/CrimeListFragment.java
@@ -7,12 +7,15 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.ListView;
@@ -91,6 +94,11 @@ public class CrimeListFragment extends ListFragment {
 		}
 	}
 
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+		getActivity().getMenuInflater().inflate(R.menu.crime_list_item_context, menu);
+	}
+	
 	@TargetApi(11)
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
@@ -129,6 +137,26 @@ public class CrimeListFragment extends ListFragment {
 				getActivity().getActionBar().setSubtitle(R.string.subtitle);
 			}
 		}
+		
+		ListView listView = (ListView)v.findViewById(android.R.id.list);
+		registerForContextMenu(listView);
+		
 		return v;
+	}
+	
+	@Override 
+	public boolean onContextItemSelected(MenuItem item) {
+		AdapterContextMenuInfo info = (AdapterContextMenuInfo)item.getMenuInfo();
+		int position = info.position;
+		CrimeAdapter adapter = (CrimeAdapter)getListAdapter();
+		Crime crime = adapter.getItem(position);
+		
+		switch (item.getItemId()) {
+		case R.id.menu_item_delete_crime:
+			CrimeLab.get(getActivity()).deleteCrime(crime);
+			adapter.notifyDataSetChanged();
+			return true;
+		}
+		return super.onContextItemSelected(item);
 	}
 }
