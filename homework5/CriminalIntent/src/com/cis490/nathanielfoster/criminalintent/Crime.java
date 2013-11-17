3bf06957a263dae86fca611c02d584fc82685c07
Nathaniel Foster <nathanielsmusic@gmail.com>
2013-11-16 19:08:20 -0500

    ch17, saving and loading data with JSONSerializer

--- homework5/CriminalIntent/src/com/cis490/nathanielfoster/criminalintent/Crime.java
+++ homework5/CriminalIntent/src/com/cis490/nathanielfoster/criminalintent/Crime.java
@@ -3,7 +3,15 @@ package com.cis490.nathanielfoster.criminalintent;
 import java.util.Date;
 import java.util.UUID;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class Crime {
+	private static final String JSON_ID = "id";
+	private static final String JSON_TITLE = "title";
+	private static final String JSON_SOLVED = "solved";
+	private static final String JSON_DATE = "date";
+	
 	private UUID mId;
 	private String mTitle;
 	private Date mDate;
@@ -14,6 +22,12 @@ public class Crime {
 		mDate = new Date();
 	}
 	
+	public Crime(JSONObject json) throws JSONException {
+		mId = UUID.fromString(json.getString(JSON_ID));
+		mTitle = json.getString(JSON_TITLE);
+		mSolved = json.getBoolean(JSON_SOLVED);
+		mDate = new Date(json.getLong(JSON_DATE));
+	}
 	@Override
 	public String toString() {
 		return mTitle;
@@ -47,6 +61,13 @@ public class Crime {
 		return mId;
 	}
 
-
+	public Object ToJSON() throws JSONException {
+		JSONObject json = new JSONObject();
+		json.put(JSON_ID, mId.toString());
+		json.put(JSON_TITLE, mTitle);
+		json.put(JSON_SOLVED, mSolved);
+		json.put(JSON_DATE, mDate.getTime());
+		return json;
+	}
 	
 }
